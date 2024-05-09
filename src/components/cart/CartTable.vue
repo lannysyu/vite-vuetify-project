15b42afecd4894323b4220621835f3348f79b5d9
LannySyu <oopsjohn.77@gmail.com>
2024-05-09 18:12:21 +0800

    cart

--- src/components/cart/CartTable.vue
+++ src/components/cart/CartTable.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { reactive, inject, ref } from "vue";
+import { reactive, inject, ref, computed } from "vue";
 import ContentHeader from "@/components/content/Content-header.vue";
 import ContentFooter from "@/components/content/Content-footer.vue";
 import logo from "@/assets/img/labubu1.jpg"; // 導入圖片
@@ -34,6 +34,10 @@ const desserts = reactive({
 });
 
 // 🚀 method
+const subtotal = computed(() => {
+    return desserts.list.reduce((acc, item) => acc + item.price * item.quantity, 0);
+});
+
 </script>
 
 <template>
@@ -41,8 +45,11 @@ const desserts = reactive({
         <thead>
             <tr>
                 <th class="text-left"></th>
-                <th class="text-left">QUANTITY</th>
-                <th class="text-left">TOTAL</th>
+                <th class="text-left text-center">QUANTITY</th>
+                <th
+                    class="text-left text-right pr-10"
+                    style="width:150px"
+                >TOTAL</th>
             </tr>
         </thead>
         <tbody>
@@ -51,22 +58,28 @@ const desserts = reactive({
                 :key="item.name"
             >
                 <td class="px-0">
-                    <div class="d-flex align-center ga-2">
-                        <div>
-                            <v-img
-                                width="150px"
-                                :src="item.img"
-                                class="ma-3"
-                            />
+                    <router-link
+                        to="/product"
+                        class="text-decoration-none text-white"
+                    >
+                        <div class="d-flex align-center ga-2">
+                            <div>
+                                <v-img
+                                    width="150px"
+                                    :src="item.img"
+                                    class="ma-3"
+                                />
+                            </div>
+                            <div class="d-flex align-center ga-2 flex-column">
+                                <div class="font-weight-bold">{{ item.name }}</div>
+                                <v-btn
+                                    color="grey-darken-3"
+                                    class="mr-auto"
+                                    size="small"
+                                >REMOVE</v-btn>
+                            </div>
                         </div>
-                        <div class="d-flex align-center ga-2 flex-column">
-                            <div class="font-weight-bold">{{ item.name }}</div>
-                            <v-btn
-                                color="grey-darken-3 mr-auto"
-                                size="small"
-                            >REMOVE</v-btn>
-                        </div>
-                    </div>
+                    </router-link>
                 </td>
                 <td class="font-weight-bold">
                     <v-text-field
@@ -75,9 +88,23 @@ const desserts = reactive({
                         v-model="item.quantity"
                         style="width:90px"
                         hide-details
+                        min="0"
                     ></v-text-field>
                 </td>
-                <td class="font-weight-bold">NT. <strong>{{ item.price * item.quantity }}</strong></td>
+                <td class="font-weight-bold text-right pr-10 text-overline">NT. <strong>{{ item.price * item.quantity
+                        }}</strong>
+                </td>
+            </tr>
+            <tr>
+                <td></td>
+                <td class="text-overline text-center">Subtotal</td>
+                <td class="text-overline font-weight-bold text-right pr-10">NT. {{ subtotal }}</td>
+            </tr>
+            <tr>
+                <v-btn
+                    color="red"
+                    size="small"
+                >Check Out</v-btn>
             </tr>
         </tbody>
     </v-table>
